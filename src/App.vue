e0c9565a6c9e696863540e7f9a8fe4b45ac349c5
minhtien020900 <50957818+cobedoimatbuon0908@users.noreply.github.com>
2020-10-27 09:26:21 +0700

    up
    up

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Dictionary/>
+</template>
+
+<script>
+import Dictionary from './components/Dictionary.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dictionary,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { BootstrapVue,BootstrapVueIcons  } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
+
+Vue.use(BootstrapVueIcons)
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
